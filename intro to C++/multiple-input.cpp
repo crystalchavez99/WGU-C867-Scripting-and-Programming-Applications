e020be97b5f5c7c64123749b1af61b65002feb8b
Crystal <crystal120599@gmail.com>
2022-08-12 10:29:36 -0700

    work for 2.2

---
+++ intro to C++/multiple-input.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int birthMonth;
+   int birthYear;
+
+   /* Your solution goes here  */
+   cin >> birthMonth;
+   cin >> birthYear;
+   cout << birthMonth << "-" << birthYear << endl;
+
+   return 0;
+}
